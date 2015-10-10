c031db6f76998ccb326f5a09e9605d725415f2c0
air <air@Macbook>
2015-10-10 22:59:13 +0800

    Add the validate logic

--- src/main/java/demo/controller/ProfileController.java
+++ src/main/java/demo/controller/ProfileController.java
@@ -1,10 +1,17 @@
 package demo.controller;
 
+import java.util.Locale;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import demo.model.ProfileForm;
+import demo.utils.USLocalDateFormatter;
 
 @Controller
 public class ProfileController {
@@ -14,8 +21,16 @@ public class ProfileController {
 	}
 
 	@RequestMapping(value = "/profile", method = RequestMethod.POST)
-	public String saveProfile(ProfileForm profileForm) {
+	public String saveProfile(@Valid ProfileForm profileForm,
+			BindingResult result) {
+		if (result.hasErrors())
+			return "profile/profilePage";
 		System.out.println("save ok" + profileForm);
 		return "redirect:/profile";
+	}
+
+	@ModelAttribute("dateFormat")
+	public String localeFormat(Locale locale) {
+		return USLocalDateFormatter.getPattern(locale);
 	}
 }
