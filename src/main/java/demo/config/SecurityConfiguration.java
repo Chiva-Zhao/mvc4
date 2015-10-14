204f9e6cf0e82836334032c12c0055aef439d364
air <air@Macbook>
2015-10-14 23:57:08 +0800

    spring basic security

---
+++ src/main/java/demo/config/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Autowired
+	public void configureAuth(AuthenticationManagerBuilder builder)
+			throws Exception {
+		builder.inMemoryAuthentication().withUser("user").password("user")
+				.roles("USER").and().withUser("admin").password("admin")
+				.roles("USER", "ADMIN");
+	}
+}
