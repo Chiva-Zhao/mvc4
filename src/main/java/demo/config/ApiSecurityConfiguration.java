3ce3b66fb9d3abdc67f8a9032f012ddac3dd3145
zhaozhenhua88@gmail.com <zhaozhenhua88@gmail.com>
2015-10-15 18:10:08 +0800

    WebSecurity

--- src/main/java/demo/config/ApiSecurityConfiguration.java
+++ src/main/java/demo/config/ApiSecurityConfiguration.java
@@ -2,15 +2,16 @@ package demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//@EnableGlobalMethodSecurity(securedEnabled = true)
+@Order(1)
+public class ApiSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureAuth(AuthenticationManagerBuilder builder) throws Exception {
 		builder.inMemoryAuthentication().withUser("user").password("user").roles("USER").and().withUser("admin").password("admin").roles("USER",
