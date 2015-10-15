4bb945c9920183b27d7c9541feb941724a0cb705
zhaozhenhua88@gmail.com <zhaozhenhua88@gmail.com>
2015-10-15 13:38:45 +0800

    objectMapper

--- src/main/java/demo/config/WebConfiguration.java
+++ src/main/java/demo/config/WebConfiguration.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -13,18 +15,20 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.util.UrlPathHelper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import demo.utils.USLocalDateFormatter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import demo.utils.USLocalDateFormatter;
 
 @Configuration
 @EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
-		registry.addFormatterForFieldType(LocalDate.class,
-				new USLocalDateFormatter());
+		registry.addFormatterForFieldType(LocalDate.class, new USLocalDateFormatter());
 	}
 
 	@Override
@@ -46,14 +50,21 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public Docket userApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.paths(path -> path.startsWith("/api/")).build();
+		return new Docket(DocumentationType.SWAGGER_2).select().paths(path -> path.startsWith("/api/")).build();
+	}
+
+	@Bean
+	@Primary
+	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+		ObjectMapper mapper = builder.createXmlMapper(false).build();
+		mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		return mapper;
 	}
 
 	@Override
 	public void configurePathMatch(PathMatchConfigurer configurer) {
 		UrlPathHelper urlPathHelper = new UrlPathHelper();
-		urlPathHelper.setRemoveSemicolonContent(true);
+		urlPathHelper.setRemoveSemicolonContent(false);
 		configurer.setUrlPathHelper(urlPathHelper);
 		configurer.setUseRegisteredSuffixPatternMatch(true);
 	}
