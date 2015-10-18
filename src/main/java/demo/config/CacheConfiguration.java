bfa2e241b41adbce20d4a61da47cb7e866336382
air <air@Macbook>
2015-10-18 11:52:28 +0800

    @EnableCaching

--- src/main/java/demo/config/CacheConfiguration.java
+++ src/main/java/demo/config/CacheConfiguration.java
@@ -3,12 +3,14 @@ package demo.config;
 import java.util.Arrays;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableCaching
 public class CacheConfiguration {
 
 	@Bean
