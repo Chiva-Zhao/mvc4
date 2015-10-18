f7a8ac9dcafe8ec6672d7fcc4a5260d9e5b69b4f
air <air@Macbook>
2015-10-18 12:05:59 +0800

    Distributed cache with Redis

--- src/main/java/demo/config/RedisConfig.java
+++ src/main/java/demo/config/RedisConfig.java
@@ -1,12 +1,35 @@
 package demo.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @Profile("redis")
 @EnableRedisHttpSession
 public class RedisConfig {
+	@Bean(name = "objectRedisTemplate")
+	public RedisTemplate<Object, Object> objectRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<Object, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		return template;
+	}
 
+	@Primary
+	@Bean
+	public CacheManager cacheManager(@Qualifier("objectRedisTemplate") RedisTemplate<Object, Object> template) {
+		RedisCacheManager cacheManager = new RedisCacheManager(template);
+		cacheManager.setCacheNames(Arrays.asList("searches"));
+		cacheManager.setDefaultExpiration(36_000);
+		return cacheManager;
+	}
 }
