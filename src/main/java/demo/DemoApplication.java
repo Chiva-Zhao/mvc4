757fdcda1f9a98cc32f52790fd982690c797fd12
air <air@Macbook>
2015-10-18 22:39:57 +0800

    ShallowEtagHeaderFilter

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,12 +1,21 @@
 package demo;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public Filter etagFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
 }
