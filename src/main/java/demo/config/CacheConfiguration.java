efdb8dc62c02bf395d4b1f13bf73d021e54c38c4
air <air@Macbook>
2015-10-18 11:57:31 +0800

    GuavaCacheManager

--- src/main/java/demo/config/CacheConfiguration.java
+++ src/main/java/demo/config/CacheConfiguration.java
@@ -1,13 +1,14 @@
 package demo.config;
 
-import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.google.common.cache.CacheBuilder;
 
 @Configuration
 @EnableCaching
@@ -15,8 +16,11 @@ public class CacheConfiguration {
 
 	@Bean
 	public CacheManager cacheManager() {
-		SimpleCacheManager cacheManager = new SimpleCacheManager();
-		cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache("searches")));
+		// SimpleCacheManager cacheManager = new SimpleCacheManager();
+		// cacheManager.setCaches(Arrays.asList(new
+		// ConcurrentMapCache("searches")));
+		GuavaCacheManager cacheManager = new GuavaCacheManager("searches");
+		cacheManager.setCacheBuilder(CacheBuilder.newBuilder().softValues().expireAfterWrite(10, TimeUnit.MINUTES));
 		return cacheManager;
 	}
 }
